2c66f774ddf9b7ef9dc11903ae50fd3cdffe4b4f
Shifat-4elements <shifat@4elements.media>
2021-07-15 16:16:22 -0400

    WIP tutorial

--- lib/sandbox_web/controllers/page_controller.ex
+++ lib/sandbox_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule SandboxWeb.PageController do
   use SandboxWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    #render(conn, "index.html")
+    conn
+    |> put_layout(false)
+    |> render("index.html")
   end
 end
